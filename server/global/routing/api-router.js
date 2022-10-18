7b0020afc7836a671081f954c25177d23bae2164
Jerome Yvan Apolonio <45879534+jerome-yvan@users.noreply.github.com>
2022-10-19 05:23:52 +0800

    Update api-router.js
    update test

--- server/global/routing/api-router.js
+++ server/global/routing/api-router.js
@@ -13,45 +13,7 @@ function requireLogin() {
    * If so, let them through. If not, block access.
    */
   return function(req, res, next) {
-    // check by token
-    if(req.headers.token) {
-      // header has token. Use it.
-      logger.debug("LOGIN CHECK HIT - by token");
-      logger.debug(req.headers.token);
-
-      User.findOne({apiToken: req.headers.token}).exec(function(err, user) {
-        if(err || !user) {
-          logger.error(err);
-          res.status(403);
-          res.send({success: false, message: "UNAUTHORIZED - INVALID TOKEN"});
-        } else {
-          logger.debug("found user by header");
-
-          // check token time period
-          if(User.tokenExpired(user.tokenCreated)) {
-            logger.debug("token is expired");
-            res.status(403);
-            res.send({success: false, message: "UNAUTHORIZED - TOKEN HAS EXPIRED"});
-          } else {
-            req.user = user;
-            logger.debug("REQ.USER 1");
-            logger.debug(req.user.username);
-            next();
-          }
-        }
-      });
-
-    } else {
-      // header does NOT have token. Check passport session
-      logger.debug("LOGIN CHECK HIT - by cookie");
-
-      // check by passport session
-      if(!req.isAuthenticated()) {
-        logger.warn("UNAUTHORIZED");
-        res.status(403);
-        res.send({success: false, message: "UNAUTHORIZED - NOT LOGGED IN"});
-      } else {  next(); }
-    }
+    
   }
 }
 
